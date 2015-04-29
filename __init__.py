aa4e90c7eb8fc0bb455cf5fe2d71a342d87945b0
S. Andrew Sheppard <andrew@wq.io>
2015-04-29 12:04:44 -0500

    update module reference

--- __init__.py
+++ __init__.py
@@ -7,14 +7,17 @@ from pkg_resources import resource_filename
 template = resource_filename('wq.start', 'django_project')
 # resource_filename not returning absolute path after pip install
 if os.sep not in template:
-    import wq
-    template = wq.__path__[0] + os.sep + template
+    import wq as wq_module
+    template = wq_module.__path__[0] + os.sep + template
 
 
 @wq.command()
 @click.argument("project_name", required=True)
 @click.argument("destination", required=False)
 def start(project_name, destination):
+    """
+    Start a new project with wq.app and wq.db
+    """
     call_command(
         'startproject',
         project_name,
