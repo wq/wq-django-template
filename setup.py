0d05f14a76f0248275a05b317925d6b424e1a7f3
S. Andrew Sheppard <andrew@wq.io>
2016-01-15 20:25:17 -0600

    update version for dev

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ for folder in TEMPLATES:
 
 setup(
     name='wq.start',
-    version='0.8.3',
+    version='1.0.0-dev',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='http://wq.io/',
