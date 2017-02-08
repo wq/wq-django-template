ff62ca0323a7b3713954070b98610399f74922af
S. Andrew Sheppard <andrew@wq.io>
2017-02-08 15:19:48 -0600

    phonegap compatibility updates

---
+++ app/js/pginit.js
@@ -0,0 +1,33 @@
+function getBaseUrl() {
+    var baseurl = window.location.pathname.replace("index.html",'');
+    baseurl = baseurl.replace(/\/$/,'');
+    if (baseurl == 'www') {
+        // Windows
+        baseurl = '/www';
+    }
+    return baseurl;
+}
+
+require.config({
+    'config': {
+        '{{ project_name }}/config': {
+             'router': {
+                 'base_url': getBaseUrl()
+             },
+             'store': {
+                 'service': 'https://{{ domain }}',
+                 'defaults': {'format': 'json'}
+             },
+        }
+    }
+});
+
+document.addEventListener('deviceready', function() {
+    require(['js/{{ project_name }}'], function() {
+        require(['{{ project_name }}/main', 'wq/app'], function(ready, app) {
+            ready.then(function() {
+                app.replaceState('');
+            });
+        });
+    });
+});
