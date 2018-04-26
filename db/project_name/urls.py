fb650d671244b0fcc9a52f2fa4fe6264c835691e
S. Andrew Sheppard <andrew@wq.io>
2018-04-26 19:15:10 +0000

    update to Django 2.0 template

--- db/project_name/urls.py
+++ db/project_name/urls.py
@@ -1,22 +1,29 @@
+"""{{ project_name }} URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 import os
-from django.conf.urls.static import static
-from django.conf.urls import include, url
-
 from django.contrib import admin
-admin.autodiscover()
-
+from django.urls import path, include
+from django.conf.urls.static import static
 from wq.db import rest
-rest.autodiscover()
-
 from django.conf import settings
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-
-    # Uncomment to enable python-social-auth URLs
-    # url(r'', include('social.apps.django_app.urls', namespace='social')),
-
-    url(r'^', include(rest.router.urls))
+    path('admin/', admin.site.urls),
+    path('', include(rest.router.urls)),
 ]
 
 if settings.DEBUG_WITH_RUNSERVER:
