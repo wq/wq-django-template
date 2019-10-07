34cae679af601794b67aa4e229e7f7bea62d7a22
S. Andrew Sheppard <andrew@wq.io>
2019-10-07 12:27:25 +0900

    update usage

--- app/js/project_name/main.js
+++ app/js/project_name/main.js
@@ -7,25 +7,14 @@ app.use(map);
 app.use(patterns);
 app.use(photos);
 
-config.presync = presync;
-config.postsync = postsync;
 var ready = app.init(config).then(function() {
     app.jqmInit();
     app.prefetchAll();
 });
 
-// Sync UI
-function presync() {
-    $('button.sync').html("Syncing...");
-    $('li a.ui-icon-minus, li a.ui-icon-alert')
-       .removeClass('ui-icon-minus')
-       .removeClass('ui-icon-alert')
-       .addClass('ui-icon-refresh');
-}
 
-function postsync(items) {
-    $('button.sync').html("Sync Now");
-    app.syncRefresh(items);
+if (config.debug) {
+    window.wq = app;
 }
 
 return ready;
