f77e016aff07e1f2d2c58cf52f49aec7d23e5b60
S. Andrew Sheppard <andrew@wq.io>
2023-02-20 16:18:16 +0900

    update urls.py

--- db/project_name/urls.py
+++ db/project_name/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from wq.db import rest
 from django.conf import settings
@@ -23,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(rest.router.urls)),
+    path('', rest.router.urls),
 ]
 
 if settings.DEBUG_WITH_RUNSERVER:
